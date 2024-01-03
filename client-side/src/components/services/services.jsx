fa78887e8f33cd7c773b54dee661fc728b9e3b00
Josy-coder <josekecha@keemail.me>
2024-01-03 10:41:56 +0200

    added the service items

--- client-side/src/components/services/services.jsx
+++ client-side/src/components/services/services.jsx
@@ -1,4 +1,11 @@
 import './services.css';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { services } from '../../Data';
+import { FaArrowRight } from 'react-icons/fa';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+
 
 const Services = () => {
   return (
@@ -7,6 +14,54 @@ const Services = () => {
         <p className='section__subtitle'>
             My <span>Services</span>
         </p>
+
+        <Swiper
+            slidesPerView={1}
+            spaceBetween={10}
+            pagination={{
+                clickable: true,
+            }}
+            breakpoints={{
+                640: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                768: {
+                    slidesPerView: 4,
+                    spaceBetween: 40,
+                },
+                1024: {
+                    slidesPerView: 5,
+                    spaceBetween: 50,
+                },
+            }}
+            modules={[Pagination]}
+            className='services__container container mySwiper'
+        >
+            {services.map(({name,title, description}, index) => {
+                return (
+                    <SwiperSlide key={index} className='services__item card card-one'>
+                        <span className='services__subtitle text-cs'>
+                            {name}
+                        </span>
+
+                        <h3 className='services__item title'>
+                            {title}
+                        </h3>
+
+                        <p className='serices__description'>
+                            {description}
+                        </p>
+
+                        <a href="" className='link'>
+                           See Pricing
+                           <FaArrowRight className='link__icon'></FaArrowRight>
+                        </a>
+
+                    </SwiperSlide>
+                )
+            })}
+        </Swiper>
     </section>
   )
 }
