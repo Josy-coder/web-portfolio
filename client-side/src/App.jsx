f8206599457bcaf997123f15214791d709aff634
Josy-coder <josekecha@keemail.me>
2024-01-03 22:02:01 +0200

    Updated the basic css and the data for the skills section

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './components/home/home';
 import Services from './components/services/services';
+import Skills from './components/skills/skills';
 
 function App() {
   return (
     <main className="main">
       <Home />
       <Services />
+      <Skills />
     </main>
   )
 }
--- client-side/src/components/skills/skills.jsx
+++ client-side/src/components/skills/skills.jsx
@@ -1,12 +1,42 @@
-import './services.css';
-
+import './skills.css';
+import { skills } from '../../Data';
 
 
 const Skills = () => {
   return (
-    <div>
-    Skills
-    </div>
+    <section className='skills section' id='skills'>
+        <h2 className='section__title text-cs'>
+            Professional Skills
+        </h2>
+        <p className='section__subtitle'>
+            My <span>Talent</span>
+        </p>
+
+        <div className='skills__container container grid'>
+            {skills.map(({name, percentage, description}, index) => {
+                return (
+                    <div key={index} className='skills__item'>
+                        <div className='skills__titles'>
+                            <h3 className='skills__name'>
+                                {name}
+                            </h3>
+                            <span className='skills__number'>
+                                {percentage} <span>%</span>
+                            </span>
+                        </div>
+                        <p className='skills__description'>
+                            {description}
+                        </p>
+                        <div className='skills__bar'>
+                            <span className='skills__percentage skills'>
+                                <span></span>
+                            </span>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    </section>
   )
 }
 
