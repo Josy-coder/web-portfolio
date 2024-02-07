7781c64f60f46862919590b1c10658420a5756c7
Josy-coder <josekecha@keemail.me>
2024-02-07 11:52:47 +0200

    Add header component with CSS and functionality

--- client-side/src/components/header/header.jsx
+++ client-side/src/components/header/header.jsx
@@ -1,10 +1,68 @@
 import './header.css';
+import { links } from '../../Data';
+import { FaDiscord, FaLinkedinIn, FaTelegramPlane } from 'react-icons/fa';
+import { BsSun, BsMoon } from 'react-icons/bs';
+import { useState } from 'react';
+
 
 const Header = () => {
+
+    const [showMenu, setShowMenu] = useState(false);
   return (
-    <div>
-        Header
-    </div>
+    <header className='header'>
+        <nav className='nav'>
+            <a href="" className='nav__log text-cs'>
+                Joselyto
+            </a>
+
+            <div
+                className={`${showMenu ? 'nav__menu show-menu' : 'nav_menu'}`}
+            >
+                <div className='nav__data'>
+                    <ul className='nav__list'>
+                        {links.map(({name, path}, index) => {
+                            return (
+                                <li className='nav__item' key={index}>
+                                    <a href={path} className="nav__link text-cs">
+                                        {name}
+                                    </a>
+                                </li>
+                            );
+                        })}
+                    </ul>
+
+                    <div className="header__socials">
+                        <a href="" className="header__social-link">
+                            <FaLinkedinIn />
+                        </a>
+
+                        <a href="" className="header__social-link">
+                            <FaDiscord />
+                        </a>
+
+                        <a href="" className="header__social-link">
+                            <FaTelegramPlane />
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div className='nav__btns'>
+                <div className='theme_toggler'>
+                    <BsSun />
+                </div>
+
+                <div
+                    className='nav__toggle'
+                    onClick={() => setShowMenu(!showMenu)}
+                >
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </nav>
+    </header>
+
   );
 };
 
