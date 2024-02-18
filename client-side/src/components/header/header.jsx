f19d69b1b537c688613dfe000860beb0e1006cba
Baho Kemana Joselyto Charite <88556861+Josy-coder@users.noreply.github.com>
2024-02-18 20:27:49 +0000

    feat: enabled react-scroll for the navbar and menu. Enabled CSS for light mode

--- client-side/src/components/header/header.jsx
+++ client-side/src/components/header/header.jsx
@@ -2,18 +2,52 @@ import './header.css';
 import { links } from '../../Data';
 import { FaDiscord, FaLinkedinIn, FaTelegramPlane } from 'react-icons/fa';
 import { BsSun, BsMoon } from 'react-icons/bs';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-scroll';
+import { animateScroll } from 'react-scroll';
 
 
 const Header = () => {
     const [showMenu, setShowMenu] = useState(false);
+    const [scrollNav, setScrollNav] = useState(false);
+    const [theme, setTheme] =useState('dark-theme');
+
+    const scrollTop = () => {
+        animateScroll.scrollToTop();
+    };
+
+    const changeNav = () => {
+        if(window.scrollY >= 80) {
+            setScrollNav(true)
+        }
+        else {
+            setScrollNav(false)
+        }
+    };
+
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeNav)
+    }, []);
+
+    useEffect(() => {
+        document.body.classList.toggle('no-scroll', showMenu)
+    }, [showMenu]);
+
+    useEffect(() => {
+        document.documentElement.className = theme;
+    }, [theme]);
 
     return (
-    <header className='header'>
+    <header className={`${scrollNav ? 'scroll-header' : ''} header`}>
         <nav className='nav'>
-            <a href="" className='nav__logo text-cs'>
+            <Link 
+                to='/'
+                onClick={scrollTop}
+                className='nav__logo text-cs'
+            >
                 Joselyto
-            </a>
+            </Link>
 
             <div
                 className={`${showMenu ? 'nav__menu show-menu' : 'nav__menu'}`}
@@ -23,9 +57,18 @@ const Header = () => {
                         {links.map(({name, path}, index) => {
                             return (
                                 <li className='nav__item' key={index}>
-                                    <a href={`#${path}`} className="nav__link text-cs">
+                                    <Link 
+                                        to={path}
+                                        offset={-150}
+                                        spy={true}
+                                        hashSpy={true}
+                                        duration={500}
+                                        smooth={true}
+                                        onClick={() => setShowMenu(!showMenu)}
+                                        className="nav__link text-cs"
+                                    >
                                         {name}
-                                    </a>
+                                    </Link>
                                 </li>
                             );
                         })}
