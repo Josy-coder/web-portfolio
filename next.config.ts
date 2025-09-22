66f6a99f856c55b89b3695bc1b4da141e696446e
Baho Kemana Joselyto Charite <88556861+Josy-coder@users.noreply.github.com>
2025-09-22 09:29:25 +0200

    (chore) updated next config to allow image from the blob store

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'oxp8pbedietshmu8.public.blob.vercel-storage.com',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
